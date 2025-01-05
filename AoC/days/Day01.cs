36136ed87e099b29a659149f7a864af183de49f9
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-01-05 17:50:37 -0500

    Add Day 1, both parts

--- AoC/days/Day01.cs
+++ AoC/days/Day01.cs
@@ -4,9 +4,36 @@ namespace AoC
     {
         public static void Run()
         {
-            // proof that branching Main() is working
-            // no AoC code here yet
-            Console.WriteLine("Day 01 Base file runs.");
+        
+            string PATH = "/home/fairchild/documents/Challenges/Advent_15/AoC/input/";
+            string floorDirs = File.ReadAllText(PATH + "input01.txt");
+
+            int floor = 0;
+            int dirsLen = floorDirs.Length;
+            bool firstBasement = true;
+            int basementDir = 0;
+            int count = 0;
+            for (int i = 0; i<dirsLen; i++) {
+
+                // Part 1 logic
+                if (floorDirs[i] == '(' ) {
+                    floor += 1;
+                } else if (floorDirs[i] == ')' ) {
+                    floor -= 1;
+                } // else, ignore
+                
+                // Part 2 logic
+                count += 1;
+                if (floor == -1 && firstBasement) {
+                    basementDir = count;
+                    firstBasement = false;      // only record the first time this occurs
+                }
+                
+            }
+
+            Console.WriteLine("Part 1: Santa should go to floor : " + floor);
+            Console.WriteLine("Part 2: First time in basement : " + basementDir);
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Advent_15
-Advent of Code 2015
+## Advent of Code 2015
 
 Using as opportunity to apply C# learning, from BSU CS 690 Software Engineering - Spring 2025.
 That said, I'm very early in learning C#.  There will be mistakes.
